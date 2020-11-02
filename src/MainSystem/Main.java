5f8e5a903ced596e1db8ac3477dd2b7478783590
MRSlothAlan <tingkwokpoon@gmail.com>
2020-11-02 21:24:53 +0800

    push the changes to the code. The dishes and the chef class needs some refactoring.

---
+++ src/MainSystem/Main.java
@@ -0,0 +1,25 @@
+package MainSystem;
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+	public static void main(String args[]) throws IOException {
+		// get the reception of the restaurant!
+		Reception reception = Reception.getReception();
+		
+		try {
+			Scanner scanner = new Scanner(new File("./src/MainSystem/myfile.txt"));
+			while (scanner.hasNextLine()) {
+				reception.input_order(scanner.nextLine());
+			}
+			
+			scanner.close();
+		}
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
